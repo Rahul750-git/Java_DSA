a3f9b0676e59b071bd681f958279c5972de001f2
Rahul750-git <varmarahul08501@gmail.com>
2026-05-29 18:24:14 +0530

    add

--- String/equal_IgnoreCaseMethod.java
+++ String/equal_IgnoreCaseMethod.java
@@ -3,7 +3,7 @@ package String;
 public class equal_IgnoreCaseMethod {
     public static void main(String[] args) {
         String str1 = "Shyam";
-        String str2 = "Shyam";
+        String str2 = "SHYAM";
         System.out.println(str1.equalsIgnoreCase(str2));
 
     }
