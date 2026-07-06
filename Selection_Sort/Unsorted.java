34774b3a6e7ddabb818b5f5a6606fcf90ae17147
Rahul750-git <varmarahul08501@gmail.com>
2026-07-06 18:27:16 +0530

    selection sort

--- Selection_Sort/Unsorted.java
+++ Selection_Sort/Unsorted.java
@@ -2,18 +2,21 @@ package Selection_Sort;
 
 public class Unsorted {
     public static void main(String[] args) {
-        int arr[] = {6,5,3,1};
+        int arr[] = { 6, 5, 3, 1 };
         int n = arr.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (arr[j] < arr[j+1]) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
-        for(int s : arr){
+        for (int s : arr) {
             System.out.println(s);
         }
     }
