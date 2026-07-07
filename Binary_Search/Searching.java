10bf1a67e09d8d5a419412c7cdea5d3299d547da
Rahul750-git <varmarahul08501@gmail.com>
2026-07-07 19:20:50 +0530

    Binary search

---
+++ Binary_Search/Searching.java
@@ -0,0 +1,23 @@
+package Binary_Search;
+
+public class Searching {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50, 60, 70, 80 };
+        int n = arr.length;
+        int target = 80;
+        int start = 0;
+        int end = n-1;
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            // compare target
+            if (arr[mid] == target) {
+                System.out.println(mid);
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+    }
+}
