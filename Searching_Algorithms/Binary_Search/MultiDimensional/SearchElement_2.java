ed1d4a5401f5bc0e864428f908188b89b8d5b01a
Rahul750-git <varmarahul08501@gmail.com>
2026-07-21 18:38:50 +0530

    Search Element in 2D Array

---
+++ Searching_Algorithms/Binary_Search/MultiDimensional/SearchElement_2.java
@@ -0,0 +1,26 @@
+package Searching_Algorithms.Binary_Search.MultiDimensional;
+
+public class SearchElement_2 {
+    public static boolean searchMatrix(int matrix[][], int target) {
+        int totalRow = matrix.length;
+        int totalCol = matrix[0].length;
+        int row = 0;
+        int col = totalCol - 1;
+        while (row < totalRow && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}; 
+        int target = 5;
+        boolean ans = searchMatrix(matrix, target);
+        System.out.println(ans);
+    }
+}
