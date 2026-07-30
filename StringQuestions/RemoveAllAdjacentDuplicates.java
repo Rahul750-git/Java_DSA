6681fc9061cb1c26ecebce17a6fe1e3a280e9682
Rahul750-git <varmarahul08501@gmail.com>
2026-07-30 16:00:34 +0530

    Remove all Adjacent Duplicate in String

---
+++ StringQuestions/RemoveAllAdjacentDuplicates.java
@@ -0,0 +1,18 @@
+package StringQuestions;
+
+public class RemoveAllAdjacentDuplicates {
+    public static void main(String[] args) {
+        String s = "abbaca";
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i <s.length();  i++) {
+            char ch = s.charAt(i);
+
+            if (ans.length() > 0 && ans.charAt(ans.length()-1) == ch) {
+                ans.deleteCharAt(ans.length()-1);
+            }else{
+                ans.append(ch);
+            }
+        }
+        System.out.println(ans);
+    }
+}
