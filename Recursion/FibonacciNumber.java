8add1863781becad8868ad4ddbc473e44db3c7a8
Rahul750-git <varmarahul08501@gmail.com>
2026-08-02 18:47:06 +0530

    Fibonacci Number

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+
+
+public class FibonacciNumber {
+    static int fib(int n){
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int ans = fib(n-1) + fib(n-2);
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        int ans = fib(n);
+        System.out.println(ans);
+    }
+    
+}
