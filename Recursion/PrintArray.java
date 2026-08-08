0c04151647d7cfb82a90e8f1ac65d4dcbb467070
Rahul750-git <varmarahul08501@gmail.com>
2026-08-08 19:41:58 +0530

    Print Array

---
+++ Recursion/PrintArray.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+
+public class PrintArray {
+
+    static void printarray(int []arr,int i){
+        if (arr.length <= i) {
+            return;
+        }
+      if (arr[i] == 0) {
+        System.out.println(arr[i]);
+      }
+      System.out.println(arr[i]);
+      printarray(arr,i + 1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+        int i = 0;
+         printarray(arr,i);
+    }
+    
+} 
