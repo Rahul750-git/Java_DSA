ba1f8d5029dfba4c728614cd8cc16d6513e910b7
Rahul750-git <varmarahul08501@gmail.com>
2026-08-09 19:16:14 +0530

    Print Digits

---
+++ Recursion/PrintDigits.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class PrintDigits {
+    static void printdigit(int num){
+        if(num == 0){
+            return;
+        }
+        int digits = num % 10;
+        num = num / 10;
+        printdigit(num);
+        System.out.println(digits);
+    }
+    public static void main(String[] args) {
+        int num = 137;
+        printdigit(num);
+    }
+}
