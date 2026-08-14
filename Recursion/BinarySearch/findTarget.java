68ea3ae6a8769a4dc1fd2d6ff645b1ba267e87b4
Rahul750-git <varmarahul08501@gmail.com>
2026-08-14 18:42:48 +0530

    target value change

--- Recursion/BinarySearch/findTarget.java
+++ Recursion/BinarySearch/findTarget.java
@@ -21,7 +21,7 @@ public class findTarget {
 
     public static void main(String[] args) {
         int arr[] = { 10, 20, 30, 40, 50, 60, 70 ,80};
-        int target = 60;
+        int target = 70;
 
         int s = 0;
         int e = arr.length - 1;
