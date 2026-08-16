2a3b8f14d879923124f3094779490304e9564935
Rahul750-git <varmarahul08501@gmail.com>
2026-08-16 18:06:09 +0530

    House Robber

---
+++ Recursion/HouseRobber.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class HouseRobber {
+
+    static int solve(int nums[],int index){
+    if(index >= nums.length){
+        return 0;
+    }
+    int includeAns = nums[index] + solve(nums,index+2);
+    int excludeAns = 0 + solve(nums,index+1);
+    int finalAns = Math.max(includeAns,excludeAns);
+    return finalAns;
+    }
+    public static int rob(int[] nums) {
+       int index = 0;
+       int ans  = solve(nums,index);
+       return ans; 
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,7,9,3,1};
+        int ans = rob(nums);
+        System.out.println(ans);
+    }
+
+}
